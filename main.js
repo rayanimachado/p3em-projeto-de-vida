ecf37e6e3566ad483be2eedb097e2bd55f4cfda7
rayanimachado <113533419+rayanimachado@users.noreply.github.com>
2024-04-10 11:57:20 -0300

    10/04/24

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0; i <  botoes.length; i++){
-botoes[i].onclick = function(){
-    
-    botoes[i].classList.add("ativo");
-}
+botoes[i].onclick = function(){  
+
 for(let j=0;j<botoes.length;j++){
-    botoes[j].classList.remove("ativo");
-}
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
+        botoes[j].classList.remove("ativo");
     }
+    
+         botoes[i].classList.add("ativo");
+        }
+        
 }
+
+
